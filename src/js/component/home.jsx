4e7268a64b86332981a6e171178c1f18c103c0dc
AkkoLeTaco <Liciaga.Angelo@live.jobcorps.org>
2022-03-30 18:11:44 +0000

    light is done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [Tlight, setLight] = useState("grey");
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="lightBody">
+			<div
+				className={
+					Tlight === "red" ? "light red selected" : "light red"
+				}
+				onClick={() => setLight("red")}></div>
+			<div
+				className={
+					Tlight === "yellow"
+						? "light yellow selected"
+						: "light yellow"
+				}
+				onClick={() => setLight("yellow")}></div>
+			<div
+				className={
+					Tlight === "green" ? "light green selected" : "light green"
+				}
+				onClick={() => setLight("green")}></div>
 		</div>
 	);
 };
